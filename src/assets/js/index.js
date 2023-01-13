a0c01a0ebc615c24fe1080eba26b0c421dd13795
alex-web-dev <alex.web-dev@mail.ru>
2023-01-13 20:03:36 +0300

    corrections for stage 2 - 4

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -5,7 +5,6 @@ import './menu.js';
 import './profile.js';
 import './stories.js';
 import './search.js';
-import './select.js';
 import './settings-input.js';
 import './popup.js';
 import './pay-lines.js';
--- src/assets/js/stories.js
+++ src/assets/js/stories.js
@@ -4,16 +4,51 @@ if ($stories) {
   const object = {
     $el: $stories,
     timer: null,
+    activeIndex: 0,
     delay: $stories.dataset.storyDelay || DEFAULT_DELAY,
     fps: 60,
   }
 
   const $close = $stories.querySelector('.stories__close');
-  $close.addEventListener('click', () => close(object));
+  $close?.addEventListener('click', () => close(object));
 
-  const $openBtns = document.querySelectorAll('.js-open-stories');
-  $openBtns.forEach($btn => {
-    $btn.addEventListener('click', () => open(object));
+  const $openLists = document.querySelectorAll('.js-open-stories-list');
+  $openLists.forEach($list => {
+    const $openBtns = $list.querySelectorAll('.js-open-stories');
+    $openBtns.forEach(($btn, index) => {
+      $btn.addEventListener('click', () => {
+        object.activeIndex = index;
+        open(object);
+      });
+    });
+  });
+
+  const $prev = $stories.querySelector('.stories__prev');
+  $prev?.addEventListener('click', () => {
+    object.activeIndex = Math.max(object.activeIndex - 1, 0);
+    toggle(object);
+  });
+
+  const $next = $stories.querySelector('.stories__next');
+  $next?.addEventListener('click', () => {
+    object.activeIndex++;
+    toggle(object);
+  });
+
+  window.addEventListener('keyup', (e) => {
+    switch(e.key) {
+      case 'Escape':
+        close(object);
+        break;
+      case 'ArrowLeft':
+        object.activeIndex = Math.max(object.activeIndex - 1, 0);
+        toggle(object);
+        break;
+      case 'ArrowRight':
+        object.activeIndex++;
+        toggle(object);
+        break;
+    }
   });
 }
 
@@ -24,18 +59,23 @@ function open(object) {
 }
 
 function handler(object) {
-  let iterator = 0;
   let progressPersent = 0;
+  let index = object.activeIndex;
   const tick = 1000 / object.fps;
   const $stories = object.$el;
   const $items = $stories.querySelectorAll('.stories__item');
   const $progressItems = $stories.querySelectorAll('.stories__progress-item');
 
-  $items[iterator].classList.add('stories__item--active');
+  for (let i = 0; i < index; i++) {
+    $progressItems[i].querySelector('.progress__done').style.width = '100%';
+  }
+
+  $items[index].classList.add('stories__item--active');
   object.timer = setInterval(() => {
-    const $thisItem = $items[iterator];
-    const $nextItem = $items[iterator + 1];
-    const $progressItem = $progressItems[iterator];
+    index = object.activeIndex;
+    const $thisItem = $items[index];
+    const $nextItem = $items[index + 1];
+    const $progressItem = $progressItems[index];
     const $done = $progressItem?.querySelector('.progress__done');
 
     progressPersent = Math.min(progressPersent + tick / object.delay * 100, 100);
@@ -45,7 +85,7 @@ function handler(object) {
       $thisItem.classList.remove('stories__item--active');
       $nextItem.classList.add('stories__item--active');
       progressPersent = 0;
-      iterator++;
+      object.activeIndex++;
     } else if (progressPersent === 100) {
       close(object);
     }
@@ -63,6 +103,27 @@ function close(object) {
 
   clearInterval(object.timer);
 
-  const $activeItem = $stories.querySelector('.stories__item--active');
-  $activeItem?.classList.remove('stories__item--active');
+  const $activeIndex = $stories.querySelector('.stories__item--active');
+  $activeIndex?.classList.remove('stories__item--active');
+}
+
+function toggle(object) {
+  let index = object.activeIndex;
+  const $activeIndex = $stories.querySelector('.stories__item--active');
+  $activeIndex?.classList.remove('stories__item--active');
+
+  const $progressItemsDone = $stories.querySelectorAll('.stories__progress-item .progress__done');
+  $progressItemsDone.forEach($done => $done.setAttribute('style', ''));
+
+  const $newActiveItem = $stories.querySelectorAll('.stories__item')[index];
+  if (!$newActiveItem) {
+    close(object);
+    return;
+  }
+
+  $newActiveItem.classList.add('stories__item--active');
+
+  clearInterval(object.timer);
+
+  handler(object);
 }
